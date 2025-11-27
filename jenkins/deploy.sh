79c627676e9096168b82d8d6a44d11689f8de23e
OscarNeiraN <o.neira@duocuc.cl>
2025-11-27 18:10:37 -0300

    first commit

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,12 +2,14 @@
 
 EC2_USER="ec2-user"
 EC2_IP="3.239.5.93"
-EC2_KEY="claves-jenkins-ubuntu.pem"
+EC2_KEY="jenkins/claves-jenkins-ubuntu.pem"
 IMAGE="oneiran/SecureDev:latest"
 
+chmod 600 $EC2_KEY
+
 ssh -o StrictHostKeyChecking=no -i $EC2_KEY $EC2_USER@$EC2_IP "
-  sudo docker stop app || true &&
-  sudo docker rm app || true &&
-  sudo docker pull $IMAGE &&
-  sudo docker run -d --name app -p 80:5000 $IMAGE
+    sudo docker stop app || true &&
+    sudo docker rm app || true &&
+    sudo docker pull $IMAGE &&
+    sudo docker run -d --name app -p 80:5000 $IMAGE
 "
